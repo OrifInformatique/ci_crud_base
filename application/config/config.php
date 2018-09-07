dd250abfa0b7c4f0f69dcc67e8d28a252a4b72f6
Didier Viret <didier.viret@orif.ch>
2018-09-07 15:05:38 +0200

    Add french language and config CI

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'http://localhost/ci_packbase';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +35,9 @@ $config['base_url'] = '';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';		// ViDi - KEEP THIS EMPTY
+								// Use .htaccess rewrite mode to add the
+								// index page in url.
 
 /*
 |--------------------------------------------------------------------------
@@ -76,7 +78,7 @@ $config['url_suffix'] = '';
 | than english.
 |
 */
-$config['language']	= 'english';
+$config['language']	= 'french';
 
 /*
 |--------------------------------------------------------------------------
@@ -324,7 +326,7 @@ $config['cache_query_string'] = FALSE;
 | https://codeigniter.com/user_guide/libraries/encryption.html
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'k7W2DvW4Y9st';
 
 /*
 |--------------------------------------------------------------------------
@@ -377,10 +379,10 @@ $config['encryption_key'] = '';
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
-$config['sess_driver'] = 'files';
+$config['sess_driver'] = 'database';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = NULL;
+$config['sess_save_path'] = 'ci_sessions';
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
@@ -400,7 +402,7 @@ $config['sess_regenerate_destroy'] = FALSE;
 |       'cookie_httponly') will also affect sessions.
 |
 */
-$config['cookie_prefix']	= '';
+$config['cookie_prefix']	= 'ci_packbase';
 $config['cookie_domain']	= '';
 $config['cookie_path']		= '/';
 $config['cookie_secure']	= FALSE;
---
+++ application/language/french/MY_application_lang.php
@@ -0,0 +1,28 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * French translations of applcation's texts
+ *
+ * @author      Didier Viret
+ * @link        https://github.com/OrifInformatique/ci_pack_base
+ * @copyright   Copyright (c), Orif <http://www.orif.ch>
+ */
+
+// Application name
+$lang['app_title']                      = 'Section informatique<br />Package de base';
+
+// Page titles
+$lang['page_prefix']                    = 'Pack base';
+
+// Date and time formats
+$lang['date_format_short']              = 'd.m.Y';
+$lang['datetime_format_short']          = 'd.m.Y H:i';
+
+// Fields labels
+
+// List headers
+
+// Buttons
+
+// Messages
+
+// Other texts
