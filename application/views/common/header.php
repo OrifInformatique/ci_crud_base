e2ea5eed137ac4404f6c72b2fe61b2b1f59e4532
Didier Viret <didier.viret@orif.ch>
2019-11-22 10:39:43 +0100

    Update logo and favicon

--- application/views/common/header.php
+++ application/views/common/header.php
@@ -18,7 +18,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     ?></title>
 
     <!-- Icon -->
-    <link rel="shortcut icon" href="<?php echo base_url("assets/images/favicon.ico"); ?>" type="image/x-icon" />
+    <link rel="shortcut icon" href="<?php echo base_url("assets/images/favicon.png"); ?>" type="image/x-icon" />
 
     <!-- Bootstrap  -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
